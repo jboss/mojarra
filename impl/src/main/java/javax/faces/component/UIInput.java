f3f910cfe6230ed3c6a536154cb24713d99f103d
Chao Wang <chaowan@redhat.com>
2020-05-20 12:23:20 +0800

    Revert "Fix problem introduced in JAVASERVERFACES_SPEC_PUBLIC-1329, also consider Submitted value "" when EMPTY_STRING_AS_NULL_PARAM_NAME is enabled."
    This reverts commit 0ed6a7eb865a573e6b6334071e7a35b8a3f38a93.

--- impl/src/main/java/javax/faces/component/UIInput.java
+++ impl/src/main/java/javax/faces/component/UIInput.java
@@ -993,8 +993,9 @@ public class UIInput extends UIOutput implements EditableValueHolder {
         // If non-null, an instanceof String, and we're configured to treat
         // zero-length Strings as null:
         //   call setSubmittedValue(null)
-        boolean isEmptyStringNull = (considerEmptyStringNull(context) && submittedValue instanceof String && ((String) submittedValue).length() == 0);
-        if (isEmptyStringNull) {
+        if ((considerEmptyStringNull(context)
+             && submittedValue instanceof String
+             && ((String) submittedValue).length() == 0)) {
             setSubmittedValue(null);
             submittedValue = null;
         }
@@ -1016,18 +1017,17 @@ public class UIInput extends UIOutput implements EditableValueHolder {
 
         // If our value is valid, store the new value, erase the
         // "submitted" value, and emit a ValueChangeEvent if appropriate
-        Object previous = getValue();
-        if (isValid() && !isEmptyStringNull) {
+        if (isValid()) {
+            Object previous = getValue();
             setValue(newValue);
             setSubmittedValue(null);
+            if (compareValues(previous, newValue)) {
+                queueEvent(new ValueChangeEvent(context, this, previous, newValue));
+            }
         } else {
             if (submittedValue == null) {
                 setSubmittedValue("");
             }
-        }
-
-        if (compareValues(previous, newValue)) {
-            queueEvent(new ValueChangeEvent(context, this, previous, newValue));
         }
 
     }
