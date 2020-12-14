rmartinc/eclipse-ee4j-mojarra

[Issue 4646] @PreDestroy not called all the times on view scoped using CDI
Signed-off-by: rmartinc <rmartinc@redhat.com>

test/javaee7/cdiInitDestroyEvent/src/main/java/com/sun/faces/test/javaee7/cdiinitdestroyevent/ViewScopedBean.java
@@ -17,6 +17,8 @@
 package com.sun.faces.test.javaee7.cdiinitdestroyevent;
 
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -25,10 +27,25 @@ import javax.inject.Named;
 public class ViewScopedBean implements Serializable {
     
     private static final long serialVersionUID = -6301337067401894253L;
+    private static int counter = 0;
     
     private String value = "My View Scoped Value";
 
     public String getValue() {
         return value;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        counter++;
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        counter--;
     }
 }

jboss/mojarra

test/javaee7/cdiInitDestroyEvent/src/main/java/com/sun/faces/test/javaee7/cdiinitdestroyevent/ViewScopedBean.java
@@ -17,6 +17,8 @@
 package com.sun.faces.test.javaee7.cdiinitdestroyevent;
 
 import java.io.Serializable;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -25,10 +27,25 @@ import javax.inject.Named;
 public class ViewScopedBean implements Serializable {
     
     private static final long serialVersionUID = -6301337067401894253L;
+    private static int counter = 0;
     
     private String value = "My View Scoped Value";
 
     public String getValue() {
         return value;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        counter++;
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        counter--;
     }
 }